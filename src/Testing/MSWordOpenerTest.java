6c1bcc3c40dcbc759d8f998d8f97048f64589e95
nikosoiko <84327434+nikosoiko@users.noreply.github.com>
2021-05-24 20:38:43 +0300

    Add files via upload

---
+++ src/Testing/MSWordOpenerTest.java
@@ -0,0 +1,44 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.junit.Test;
+
+import fileOpener.MSWordOpener;
+
+
+public class MSWordOpenerTest {
+
+	@Test
+	public void testGetFileContents() {
+		
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		File f = chooser.getSelectedFile();
+
+		
+		try {
+            FileInputStream inputFile = new FileInputStream(f.getAbsolutePath());
+            try (XWPFDocument document = new XWPFDocument(inputFile)) {
+				List<XWPFParagraph> paragraphs = document.getParagraphs();
+				for (int i = 0; i < paragraphs.size(); i++) {
+				}
+			}
+            inputFile.close();
+
+        } catch (Exception e1) {
+            System.out.println("File not found \ngeneral I/O Error");
+            
+        }
+		
+	}
+
+}
