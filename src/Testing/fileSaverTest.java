be29933377166aed1e06eeb82b5b988f1f189893
nikosoiko <84327434+nikosoiko@users.noreply.github.com>
2021-05-25 12:00:17 +0300

    Add files via upload

---
+++ src/Testing/fileSaverTest.java
@@ -0,0 +1,39 @@
+package Testing;
+
+import fileSaver.saverFactory;
+import fileSaver.saverInterface;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import fileSaver.saverFactory;
+import fileSaver.saverInterface;
+
+public class fileSaverTest {
+	
+	@Test
+	public void wordSaverTest() {
+
+		saverInterface saver = saverFactory.createSaver("word");
+		boolean outputTest = saver.writeContentsToFile((new File("./AdvancedTextToSpeechApp-master/Test/outputTest.docx")) , "Hi!!" ); //provided test file
+
+		assertEquals(outputTest, true);
+	}
+	
+	@Test
+	public void excelSaverTest() {
+
+		saverInterface saver = saverFactory.createSaver("excel");
+		boolean outputTest = saver.writeContentsToFile((new File("./AdvancedTextToSpeechApp-master/Test/outputTest.xlsx")) , "TEST 3\tTEST 4\tTEST 5\t \n" +
+                "12\t13\t14\t \n" +
+                "15\t16\t17\t \n" +
+                "18\t19\t20\t \n");
+
+		assertEquals(outputTest, true);
+	}
+	
+
+}
